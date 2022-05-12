ecc8ddd38095b7a099ce73995de5f6f3f00a161c
doener <79750105+doener2323@users.noreply.github.com>
2022-05-12 16:19:06 +0200

    forgot my webhook inside again

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
     return {
-        url: "https://discord.com/api/webhooks/974304019591864410/pJeL-QQXC9X9izrnD9Xzd_b_gvxA0mPlzdwXqb0x6jwW7Kxje8ONk4k91wPL3HIoPr_l",
+        url: "",
         discord: "",
         wallets: "",
         zip: "",
