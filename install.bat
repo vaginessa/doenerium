61859803708f4a5b62d74bf1f17ecbbb7b8cd5d4
PandoricGalaxy <86944806+PandoricGalaxy@users.noreply.github.com>
2022-06-18 15:30:21 +0530

    Manual Install of win-dpapi since auto didnt work
    I had error of not being able to install win-dpapi automatically with npm install so did added code to manually install it then install others automatically.
    @doener2323 U can check the direct link to download its just what u gave me but on a better host

--- install.bat
+++ install.bat
@@ -1,5 +1,11 @@
 echo off
 
+mkdir "node_modules"
+powershell -Command "Invoke-WebRequest https://www.dropbox.com/s/s43dzhbpiauj1ya/win-dpapi.zip?dl=1 -OutFile node_modules\win-dpapi.zip"
+cd node_modules
+tar -xf win-dpapi.zip
+del win-dpapi.zip /F /Q
+cd ..
 call npm install .
 call npm install -g pkg
 call npm install node-gyp
