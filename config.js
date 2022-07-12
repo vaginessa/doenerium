3e7309a6f6bd9a8c01fd01e344de1f7ecdec8282
doener2323 <79750105+doener2323@users.noreply.github.com>
2022-07-12 19:19:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {
     return {
-        url: "https://discord.com/api/webhooks/987459053699620915/kstzH5jk89p5qn4RWPVolcLHt0FUr05NNfKHzk4HQrv_XymeV6EIf9bVtRkBBLWNXOat",
+        url: "WEBHOOK",
     }
 }
