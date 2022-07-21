43d0f2c1f0ec242842b5622ca3b88d28f3687994
doener2323 <79750105+doener2323@users.noreply.github.com>
2022-07-21 23:28:34 +0300

    big fix

--- install.bat
+++ install.bat
@@ -1,11 +1,9 @@
 echo off
 
-mkdir "node_modules"
-powershell -Command "Invoke-WebRequest https://www.dropbox.com/s/s43dzhbpiauj1ya/win-dpapi.zip?dl=1 -OutFile node_modules\win-dpapi.zip"
-cd node_modules
-tar -xf win-dpapi.zip
-del win-dpapi.zip /F /Q
-cd ..
+echo NOTE: Make sure you have NodeJS and Visual Studio with all C++ prequisites installed. Otherwise this won't work
+
 call npm install .
 call npm install -g pkg
 call npm install node-gyp
+call npm install win-dpapi
+call npm rebuild
